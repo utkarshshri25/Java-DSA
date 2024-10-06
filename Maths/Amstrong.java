f3b2a9d70b901862ff0385096cc7e99410b04ddb
Ishan Shrivastava <utkarshcoder@gmail.com>
2024-10-06 07:58:35 +0530

    amstrong update

--- Maths/Amstrong.java
+++ Maths/Amstrong.java
@@ -1,17 +1,17 @@
-//Number is amstrong if abc= a^3 + b^3 + c^3 , abcd= a^4 + b^4 + c^4 + d^4
+//Number is amstrong if abc= a^3 + b^3 + c^3 , abcd= a^3 + b^3 + c^3 + d^3
 
 public class Amstrong {
     
     public static void CheckIfAmstrongnumber(int x)
     {
-        int length= String.valueOf(x).length(); //to find number of digits in a number
+       
         int copy=x;
         int sum=0;
 
         while (x>0)
         {
             x=x%10;
-            sum = sum + (int)Math.pow(x,length); 
+            sum = sum + (int)Math.pow(x,3); 
             x=x%10;
 
         }

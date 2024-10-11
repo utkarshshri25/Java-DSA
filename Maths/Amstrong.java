261a42a944259d82a16b5df95f0267e73654e7fa
utkarshcoder <59562051+utkarshshri25@users.noreply.github.com>
2024-10-11 16:57:23 +0530

    correction

--- Maths/Amstrong.java
+++ Maths/Amstrong.java
@@ -1,4 +1,4 @@
-//Number is amstrong if abc= a^3 + b^3 + c^3 , abcd= a^3 + b^3 + c^3 + d^3
+//Number is amstrong if abc= a^3 + b^3 + c^3 , abcd= a^4 + b^4 + c^4 + d^4
 
 public class Amstrong {
     
@@ -6,12 +6,21 @@ public class Amstrong {
     {
        
         int copy=x;
+        int x1=x;            //store the number in another variable to count number of digits
         int sum=0;
+        int count=0;
+
+
+        while (x1>0)
+        {
+            x1/=10;
+            count++;
+        }
 
         while (x>0)
         {
             x=x%10;
-            sum = sum + (int)Math.pow(x,3); 
+            sum = sum + (int)Math.pow(x,count); 
             x=x/10;
 
         }
---
+++ Maths/FindFactors.java
@@ -0,0 +1,16 @@
+/*
+n=36
+
+1*36
+2*18
+3*12
+4*9
+6*6
+-------6 is sqrt of 36 || after sqrt of n, factors start repeating || If i is one factor of n, n/i is other factor
+9*4
+12*3
+18*2
+36*1
+*/
+
+

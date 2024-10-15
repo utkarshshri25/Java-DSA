78f24c5954c58f86e41e56a0da543c3782e52d38
Ishan Shrivastava <utkarshcoder@gmail.com>
2024-10-16 00:45:58 +0530

    factors-logN

--- Maths/FindFactors.java
+++ Maths/FindFactors.java
@@ -20,10 +20,20 @@ public class FindFactors {
     public static void main(String[] args) {
         
         int n=36;
-        ArrayList <Integer> factors= ArrayList<Integer>();
-        int sqrt=Math.sqrt(n);
-        
-        
+        ArrayList <Integer> factors= new ArrayList<>();  //we are taking arraylist coz count of factors is not known
+        int sqrtN = (int) Math.sqrt(n);
 
+        for(int i=1; i<=sqrtN; i++)
+        {
+            if (n%i==0)
+            {
+                factors.add(i);
+                if (n/i != i)
+                {
+                    factors.add(n/i);
+                }
+            }
+        }
+        System.out.println(factors);
     }
 }

065613646f5bf5966bced46c050bfff6e272868f
Ishan Shrivastava <utkarshcoder@gmail.com>
2024-10-19 03:33:06 +0530

    update

--- Maths/AllPrimeFactorsOfNumber1.java
+++ Maths/AllPrimeFactorsOfNumber1.java
@@ -1,3 +1,4 @@
+//Striver
 
 import java.util.ArrayList;
 
@@ -11,12 +12,12 @@ public class AllPrimeFactorsOfNumber1 {
 
        public static ArrayList<Integer> primeFactors(int n)
     {
-        ArrayList <Integer> list2= new ArrayList<>();
-        for (int i=2; i*i <= n; i++ )
+        ArrayList <Integer> list2= new ArrayList<>();       //This function os taking sqrt(n)*2*sqrt(n) time
+        for (int i=2; i*i <= n; i++) //Time- sqrt(n)
         {
             if(n%i==0)
             {
-                if(isPrime(i))
+                if(isPrime(i)) //Time-sqrt(n)
                   list2.add(i);
                 if(n/i!=i)
                 {
@@ -45,3 +46,5 @@ public class AllPrimeFactorsOfNumber1 {
 
 
 }
+
+//Time- srqt(n) * [2*sqrt(n)]. Hence Time complex=n
